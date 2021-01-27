a7855f85c499a948d497a12228426c55ed6a2e14
Nikita Galushkin <gavrik_nikitka@mail.ru>
2021-01-27 13:27:25 +0300

    fix modile

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './SearchForm.css';
 
 function SearhForm() {
+
   return (
     <form className="search-form">
       <input placeholder="Введите тему новости" className="search-form__input" required></input>
